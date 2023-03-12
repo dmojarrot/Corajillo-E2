1b6229070234b29311bf29386e88fbcf345aea10
dmojarrot <a01638460@itesm.mx>
2023-03-12 14:45:17 -0600

    added corajillo logo

--- dashboard-eq2/src/components/MissingProductionTable.jsx
+++ dashboard-eq2/src/components/MissingProductionTable.jsx
@@ -39,7 +39,7 @@ function MissingProductionTable({ missingProduction }) {
           + Extra
         </button>
       </div>
-      <div className="overflow-x-auto overflow-y-auto sm:-mx-6 lg:-mx-8">
+      <div className="overflow-x-auto overflow-y-auto sm:-mx-6 lg:-mx-8 overflow-auto">
         <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
           <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
             <table className="min-w-full divide-y divide-gray-200  overflow-visible">
--- dashboard-eq2/src/components/NavBar.jsx
+++ dashboard-eq2/src/components/NavBar.jsx
@@ -1,14 +1,17 @@
 import React from "react"
 import { Fragment } from "react"
 import { Disclosure, Menu, Transition } from "@headlessui/react"
-import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline"
+import {
+  Bars3Icon,
+  BellIcon,
+  XMarkIcon,
+  UserCircleIcon,
+} from "@heroicons/react/24/outline"
 import { useRouter } from "next/router"
 
 const user = {
   name: "Tom Cook",
   email: "tom@example.com",
-  imageUrl:
-    "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
 }
 
 const userNavigation = [
@@ -42,8 +45,8 @@ function NavBar() {
                 <div className="flex items-center">
                   <div className="flex-shrink-0">
                     <img
-                      className="h-8 w-8"
-                      src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
+                      className="h-8 w-24"
+                      src="/logo.png"
                       alt="Your Company"
                     />
                   </div>
@@ -80,13 +83,9 @@ function NavBar() {
                     {/* Profile dropdown */}
                     <Menu as="div" className="relative ml-3">
                       <div>
-                        <Menu.Button className="flex max-w-xs items-center rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                        <Menu.Button className="flex max-w-xs items-center rounded-full bg-gray-800 text-gray-400 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
                           <span className="sr-only">Open user menu</span>
-                          <img
-                            className="h-8 w-8 rounded-full"
-                            src={user.imageUrl}
-                            alt=""
-                          />
+                          <UserCircleIcon className="h-7 w-7" />
                         </Menu.Button>
                       </div>
                       <Transition
@@ -155,10 +154,9 @@ function NavBar() {
               <div className="border-t border-gray-700 pt-4 pb-3">
                 <div className="flex items-center px-5">
                   <div className="flex-shrink-0">
-                    <img
+                    <UserCircleIcon
                       className="h-10 w-10 rounded-full"
-                      src={user.imageUrl}
-                      alt=""
+                      color="white"
                     />
                   </div>
                   <div className="ml-3">
